6f3003c3776b500866b259c91363179e08d9eddc
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-03-01 10:31:42 +0530

    Create budget.c

---
+++ budget.c
@@ -0,0 +1,6 @@
+Rent: ₹20,000 - ₹30,000
+Food: ₹3,000 - ₹7,000
+Transportation: ₹2,000 - ₹5,000
+Utilities & Internet: ₹2,500 - ₹4,500
+Miscellaneous: ₹3,000 - ₹6,000
+Total: ₹30,500 - ₹52,500/month
